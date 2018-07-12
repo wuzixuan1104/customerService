2ead302b3c5c1c37de55ddc1904e684c126832c7
吳姿萱 <cherry51120@gmail.com>
2018-07-12 15:58:10 +0800

    fiux

--- app/controller/api/Line.php
+++ app/controller/api/Line.php
@@ -50,9 +50,10 @@ class Line extends ApiController {
           $pattern = !preg_match ('/\(\?P<k>.+\)/', $pattern) ? '/(?P<k>(' . $pattern . '))/i' : ('/(' . $pattern . ')/i');
           preg_match_all ($pattern, $log->text, $result);
           Log::info('text');
-          if ($result['k'] && $msg = LineTool::start() ) {
+          if ($result['k'] ) {
             Log::info('token:' . $event->getReplyToken());
-            $msg->reply($event->getReplyToken());
+            if($msg = LineTool::start())
+              $msg->reply($event->getReplyToken());
           }
           Log::info('end');
           // if( $msg = ForexProcess::getCalcResult($source, $event->getText()) )
