d4a41709fc70e1ade9cbec14c090faf71ca68bbc
吳姿萱 <cherry51120@gmail.com>
2018-07-12 16:38:15 +0800

    fiux

--- sys/core/Model.php
+++ sys/core/Model.php
@@ -34,8 +34,8 @@ if (!function_exists ('use_model')) {
     class Model extends ActiveRecord\Model {
 
       public static function create ($attributes, $validate = true, $guard_attributes = true) {
-        var_dump(self::table ()->columns);
-        
+        var_dump(static::table ()->columns);
+
         $attributes = array_intersect_key ($attributes, self::table ()->columns);
         return parent::create ($attributes, $validate, $guard_attributes);
       }
