9475da13641ff14266b6c69e5572dd56f0be227b
吳姿萱 <cherry51120@gmail.com>
2018-07-16 18:15:31 +0800

    r

--- app/migration/021_add_webhook.php
+++ app/migration/021_add_webhook.php
@@ -8,17 +8,17 @@
  */
 
 return array (
-    'up' => "CREATE TABLE `servicers` (
+    'up' => "CREATE TABLE `webhooks` (
         `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
         `type` enum('commentCard') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'commentCard' COMMENT 'commentCard',
-        `model_id` varchar(50) unsigned NOT NULL COMMENT 'model ID',
-        `servicer_id` int(11) unsigned NOT NULL COMMENT 'trello人員ID',
+        `model_id` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'model ID',
+        `servicer_id` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'trello人員ID',
         `content` text COLLATE utf8mb4_unicode_ci COMMENT '內容',
         `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '新增時間',
         `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新時間',
         PRIMARY KEY (`id`),
-        KEY `key_id_index` (`key_id`)
+        KEY `model_id_index` (`model_id`)
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;",
-    'down' => "DROP TABLE `servicers`;",
+    'down' => "DROP TABLE `webhooks`;",
     'at' => "2018-07-16 17:59:04",
   );
