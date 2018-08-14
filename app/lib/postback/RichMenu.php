29c2ecd7d2bd0d4d0507ab24b5531fb5c65c3ee7
吳姿萱 <cherry51120@gmail.com>
2018-08-14 17:03:31 +0800

    對話紀錄

--- app/lib/postback/RichMenu.php
+++ app/lib/postback/RichMenu.php
@@ -111,7 +111,7 @@ class Qa {
       $flexes[] = FlexText::create($history->content)->setSize('xs');
       $flexes[] = FlexSeparator::create()->setMargin('xxl');
     }
-    $flexes[] = FlexButton::create('primary')->setColor('#f97172')->setHeight('sm')->setGravity('center')->setAction(FlexAction::postback('回覆訊息後按此送出', '您按了送出訊息', json_encode(['lib' => 'trello/Send', 'class' => 'Trello', 'method' => 'reply', 'param' => [])));
+    $flexes[] = FlexButton::create('primary')->setColor('#f97172')->setHeight('sm')->setGravity('center')->setAction(FlexAction::postback('回覆訊息後按此送出', '您按了送出訊息', json_encode(['lib' => 'trello/Send', 'class' => 'Trello', 'method' => 'reply', 'param' => []])));
     
     return MyLineBotMsg::create()->flex('檢視問題內容', FlexBubble::create([
             'header' => FlexBox::create([FlexText::create($title)->setWeight('bold')->setSize('md')->setColor('#e8f6f2')])->setSpacing('xs')->setLayout('horizontal'),
